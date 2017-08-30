da6e295ce91f64beb53730c0dbbd171298a1b7fe
OIH <OIH@VIA.DK>
2017-08-30 07:22:45 +0200

    Added iterator to list

--- ADT/src/dk/via/collections/List.java
+++ ADT/src/dk/via/collections/List.java
@@ -1,9 +1,12 @@
 package dk.via.collections;
 
+import java.util.Iterator;
+
 public interface List<T> {
+	T get(int i);
+	Iterator<T> iterator();
 	void add(T x);
 	void add(int i, T x);
-	T get(int i);
 	void put(int i, T x);
 	void remove(int i);
 	int size();
